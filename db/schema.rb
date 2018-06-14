6d60d31e8664a4756788ea2a1b7416e77a2ca701
Kurubushi Sox <kurubushi@sox.test>
2018-06-14 23:35:02 +0900

    [add]Favorite model 作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180608095317) do
+ActiveRecord::Schema.define(version: 20180614130612) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "recipe_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["recipe_id"], name: "index_favorites_on_recipe_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
   create_table "post_images", force: :cascade do |t|
     t.string   "mime_type"
