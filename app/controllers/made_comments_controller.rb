3cd0eefe48e2250f204e05f0171634fc9389a0e3
Kurubushi Sox <kurubushi@sox.test>
2018-06-18 23:09:23 +0900

    [update]作ったよレポートの投稿機能を追加

--- app/controllers/made_comments_controller.rb
+++ app/controllers/made_comments_controller.rb
@@ -1,5 +1,15 @@
 class MadeCommentsController < ApplicationController
   def create
+    @made_comment = MadeComment.new(
+      user_id:   current_user.id,
+      recipe_id: params[:recipe_id],
+      content:   params[:made_comment][:content]
+    )
+    if @made_comment.save
+      redirect_to :back, notice: '作ったよレポートを投稿しました'
+    else
+      redirect_to :back, alert: '作ったよレポートを投稿できませんでした。'
+    end
   end
 
   def destroy
--- app/models/made_comment.rb
+++ app/models/made_comment.rb
@@ -1,4 +1,10 @@
 class MadeComment < ActiveRecord::Base
   belongs_to :user
   belongs_to :recipe
+  before_save :verify_user
+
+  private
+    def  verify_user
+      self.user_id != self.recipe.user_id
+    end
 end
