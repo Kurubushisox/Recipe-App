427c83a0f61209f2fc3244ef887edb5fc1060649
Kurubushi Sox <kurubushi@sox.test>
2018-06-23 17:42:23 +0900

    [update]作ったよレポートへレシピ作者から返信できる機能を追加

--- app/controllers/made_comment_replies_controller.rb
+++ app/controllers/made_comment_replies_controller.rb
@@ -1,7 +1,25 @@
 class MadeCommentRepliesController < ApplicationController
   def create
+    @made_comment_reply = MadeCommentReply.new(made_comment_reply_params)
+    if @made_comment_reply.save
+      redirect_to :back, notice: '作ったよレポートに返信しました'
+    else
+      redirect_to :back, alert: '返信を投稿できませんでした'
+    end
   end
 
   def destroy
   end
+
+  private
+    def made_comment_reply_params
+      params
+      .require(:made_comment_reply)
+      .permit(
+        MadeCommentReply::REGISTRABLE_ATTRIBUTES,
+      ).merge(
+        user_id: current_user.id,
+        made_comment_id: params[:made_comment_id]
+      )
+    end
 end
--- app/models/made_comment_reply.rb
+++ app/models/made_comment_reply.rb
@@ -2,4 +2,12 @@ class MadeCommentReply < ActiveRecord::Base
   REGISTRABLE_ATTRIBUTES = %i(content)
   belongs_to :user
   belongs_to :made_comment
+
+  before_save :verify_user?
+
+  private
+    def  verify_user?
+      self.user_id = self.made_comment.recipe.user_id
+      return true
+    end
 end
